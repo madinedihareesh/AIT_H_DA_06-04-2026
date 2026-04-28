eb3087fc8c8f9794d68e03405a8f3ade02926b88
Hareesh <hareesh.madinedi@gmail.com>
2026-04-28 08:29:53 +0530

    Day13_list

---
+++ Day13_list/ex.py
@@ -0,0 +1,44 @@
+# l=[1,2,3,4,5,6,7] ##homo
+# l1=[12,12.59,9+10j,True,'achieversit'] ##hetro
+# l2=[1,2,3,4]
+# print(id(l2))
+# l2.append(5)
+# l2.append(2)
+# print(l2)
+# print(id(l2))
+
+# for i in l2:
+#     print(i)
+
+# l1=[1,2,3]
+# print(l1+[4,5])
+
+# print(l1*3)
+
+# splicing
+
+# l1=[1,2,3,4,5,6,7,8,9,10]
+# l2=l1[0:5]
+# print(l2)
+# l3=l1[::-1]
+# print(l3)
+# l1[0:3]=[11,12,13]
+
+# l1[100:100]=[101]
+
+# l4=[1,2,3,4,5]
+# l4[::-1]=[25,24,23,22,21]
+# print(l4)
+
+l1=[1,2,3,4,5,6,7]
+l2=[]
+l3=list('python')
+print(l3)
+l4=list((1,2,3,4,5,6))
+print(l4)
+
+
+a=[1,2,3]
+b=[1,2,3,4]
+print(a>b)
+
